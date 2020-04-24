07b59d28c4d7fde25239888a2f1d8446f11099dd
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2020-04-25 00:55:43 +0300

    Update screen logic

--- template/Game/HomeController.swift
+++ template/Game/HomeController.swift
@@ -3,16 +3,27 @@ import UIKit
 class HomeController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
         
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        playButton.setTitle(
-            NSLocalizedString("Play Now", comment: "Play Now"),
-            for: []
-        )
-    }
-    
     @IBAction func processPlay() {
-        navigationController?.pushViewController(GameContainer(), animated: true)
+        let controller = GameContainer()
+        controller.view.frame = view.bounds
+        controller.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(controller.view)
+        addChild(controller)
+        controller.didMove(toParent: self)
+    }
+}
+
+extension UIImage {
+    static var appIcon: UIImage? {
+        guard 
+            let iconsDictionary = Bundle.main.infoDictionary?["CFBundleIcons"] as? [String:Any],
+            let primaryIconsDictionary = iconsDictionary["CFBundlePrimaryIcon"] as? [String:Any],
+            let iconFiles = primaryIconsDictionary["CFBundleIconFiles"] as? [String],
+            let lastIcon = iconFiles.last
+        else  {
+            return nil
+        }
+        
+        return UIImage(named: lastIcon)
     }
 }
